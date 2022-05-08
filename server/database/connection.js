f231de71ec644d1c8cbef7356697634ff9c1860d
VartanBabayan <babayan.vartan@student.rau.am>
2022-05-08 21:47:17 +0400

    crud application

---
+++ server/database/connection.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const connectDB = mongoose.connect('mongodb://localhost/project', 
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+
+// const connectDB = async () => {
+//     try{
+//         // mongodb connection string
+//         const con = await mongoose.connect(process.env.MONGO_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             useFindAndModify: false,
+//             useCreateIndex: true
+//         })
+
+//         console.log(`MongoDB connected : ${con.connection.host}`);
+//     }catch(err){
+//         console.log(err);
+//         process.exit(1);
+//     }
+// }
+
+module.exports = connectDB

fce793befe7514bbbbad987d78770874f5451afe
VartanBabayan <babayan.vartan@student.rau.am>
2022-05-09 16:30:00 +0400

    mongodb connection

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
 
+// mongodb connection
 const connectDB = require('./server/database/connection');
 
 const app = express();
@@ -13,9 +14,6 @@ const PORT = process.env.PORT || 8080
 
 // log requests
 app.use(morgan('tiny'));
-
-// mongodb connection
-// connectDB;
 
 // parse request to body-parser
 app.use(bodyparser.urlencoded({ extended : true}))
@@ -32,4 +30,4 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 // load routers
 app.use('/', require('./server/routes/router'))
 
-app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`)});
